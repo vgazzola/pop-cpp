6156645cecff62c0cefb9b186f8f74209c8224df
vgazzola <valentin.gazzola@gmail.com>
2015-11-24 08:55:05 -0800

    added the interest network class, modified to cope with the new code style

---
+++ include/core/interest_network.h
@@ -0,0 +1,183 @@
+/**
+ * @file	interest_network.h
+ * @brief	Network of interest
+ * @author	Loïc Monney
+ * @version	1.0
+ */
+
+#ifndef INTEREST_NETWORK_H_
+#define INTEREST_NETWORK_H_
+
+#include "pop_base.h"
+#include "pop_buffer.h"
+#include "pop_accesspoint.h"
+#include <list>
+#include <string>
+
+/**
+ * @class	InterestNetwork
+ * @brief	An interest network is used with a given application and allows
+ * 			its users to share resources between computers in the same
+ * 			interest network.
+ *
+ * 			Each interest network is defined with:
+ * 				- a unique id which identifies the network
+ * 				- a list of friends
+ */
+class InterestNetwork : public POPBase
+{
+	private:
+		std::string _id;
+		std::list<pop_accesspoint> _friends;
+
+	public:
+
+		/**
+		 * @brief Constructor
+		 *
+		 * Constructs a new empty InterestNetwork
+		 */
+		InterestNetwork();
+
+		/**
+		 * @brief Constructor
+		 *
+		 * Constructs a new InterestNetwork based on the given parameter
+		 */
+		InterestNetwork(const InterestNetwork &p);
+
+		/**
+		 * @brief Destructor
+		 *
+		 * Destructs this InterestNetwork
+		 */
+		~InterestNetwork();
+
+		/**
+		 * @brief Compare this network of interest with the one given in parameter
+		 *
+		 * @return true if the two networks have the same id
+		 */
+		bool operator ==(const InterestNetwork &p) const;
+
+		/**
+		 * @brief Assignment
+		 */
+		InterestNetwork& operator =(const InterestNetwork &p);
+
+		/**
+		 * @brief Assignment
+		 */
+		InterestNetwork& operator =(const InterestNetwork* const p);
+
+		/**
+		 * @brief Serialize and unserialize this InterestNetwork to/from the given buffer
+		 *
+		 * @param buf	:	the buffer
+		 * @param pack	:	true for serialize, false for unserialize
+		 */
+		virtual void Serialize(POPBuffer &buf, bool pack);
+
+		/**
+		 * @brief Change the id
+		 */
+		void setId(const POPString &id);
+
+		/**
+		 * @brief Get the id of this network
+		 */
+		POPString getId() const;
+
+		/**
+		 * @brief Add the reference to a friend
+		 *
+		 * @param friendContact   : ip address + port + protocol (e.g. socket://10.0.0.11:2711)
+		 */
+		void addFriend(const std::string &friendContact);
+
+		/**
+		 * @brief Add the reference to a friend
+		 *
+		 * @param friendAccessPoint : access point of the friend's job manager
+		 */
+		void addFriend(const pop_accesspoint &friendAccessPoint);
+
+		/**
+		 * @brief Remove the given friend
+		 *
+		 * @param friendContact  : ip address + port + protocol (e.g. socket://10.0.0.11:2711)
+		 */
+		void removeFriend(const std::string &friendContact);
+
+		/**
+		 * @brief Remove the given friend
+		 *
+		 * @param friendAccessPoint : access point of the friend's job manager
+		 */
+		void removeFriend(const pop_accesspoint &friendAccessPoint);
+
+		/**
+		 * @brief Get the list of all friends that are registred in this network
+		 *
+		 * @return the list of all friends
+		 */
+		const std::list<pop_accesspoint> getFriends() const;
+};
+
+/**
+ * @class	InterestNetworkCollection
+ * @brief	Collection of network of interests
+ *
+ * 			This class is useful to encapsulate a list of network of interest.
+ * 			Thus it may be sent between POP objects
+ */
+class InterestNetworkCollection : public POPBase
+{
+	private:
+		std::list<InterestNetwork> _interests;
+
+	public:
+		InterestNetworkCollection();
+		InterestNetworkCollection(const std::list<InterestNetwork>& interests);
+
+		/**
+		 * @brief get the list of networks of friends contained in this collection
+		 *
+		 * @return list of networks of friends
+		 */
+		std::list<InterestNetwork> get() const;
+
+		/**
+		 * @brief change the list of networks of friends contained in this collection
+		 *
+		 * @param interests :   the new list
+		 */
+		void set(const std::list<InterestNetwork>& interests);
+
+		/**
+		 * @brief Serialize and unserialize this collection to/from the given buffer
+		 *
+		 * @param buf	:	the buffer
+		 * @param pack	:	true for serialize, false for unserialize
+		 */
+		virtual void Serialize(pop_buffer &buf, bool pack);
+
+		/**
+		 * @brief load and replace the list of networks of friends contained in this
+		 * 		  collection with the one stored in the given file
+		 *
+		 * @return true if the file has been successfully loaded
+		 */
+		bool loadFromConfiguration(const char* file);
+
+		/**
+		 * @brief store the list of networks of friends contained in this
+		 * 		  collection to the given file
+		 *
+		 * @return true if the file has been successfully stored
+		 */
+		bool saveToConfiguration(const char* file);
+};
+
+
+#endif /* INTEREST_NETWORK_H_ */
---
+++ lib/core/interest_network.cc
@@ -0,0 +1,215 @@
+
+#include "interest_network.h"
+#include "popc_logger.h"
+#include "pugixml.h"
+
+InterestNetwork::InterestNetwork()
+{
+}
+
+InterestNetwork::InterestNetwork(const InterestNetwork& p)
+{
+	_id = p._id;
+	_friends = p._friends;
+}
+
+InterestNetwork::~InterestNetwork()
+{
+}
+
+bool InterestNetwork::operator ==(const InterestNetwork& p) const
+{
+	return strcmp((const char*) _id, (const char*) p._id) == 0;
+}
+
+InterestNetwork& InterestNetwork::operator =(const InterestNetwork& p)
+{
+	if (this != &p)
+	{
+		_id = p._id;
+		_friends = p._friends;
+	}
+	return *this;
+}
+
+InterestNetwork& InterestNetwork::operator =(const InterestNetwork* const p)
+{
+	if (this != p)
+	{
+		_id = p->_id;
+		_friends = p->_friends;
+	}
+	return *this;
+}
+
+void InterestNetwork::Serialize(pop_buffer &buf, bool pack)
+{
+	/* serialize */
+	if(pack)
+	{
+		buf.Pack(&_id, 1);
+
+		int friendsNb = _friends.size();
+		buf.Pack(&friendsNb, 1);
+		for(std::list<pop_accesspoint>::iterator i = _friends.begin(); i != _friends.end() ; i++)
+		{
+			i->Serialize(buf, pack);
+		}
+	}
+
+	/* unserialize */
+	else
+	{
+		buf.UnPack(&_id, 1);
+
+		int friendsNb;
+		buf.UnPack(&friendsNb, 1);
+		_friends.clear();
+		for(int i = 0; i < friendsNb; i++)
+		{
+			pop_accesspoint ap;
+			ap.Serialize(buf, pack);
+			_friends.push_back(ap);
+		}
+	}
+}
+
+void InterestNetwork::setId(const std::string &id)
+{
+	_id = id;
+}
+
+std::string InterestNetwork::getId() const
+{
+	return _id;
+}
+
+void InterestNetwork::addFriend(const std::string &friendContact)
+{
+	pop_accesspoint ap;
+	ap.SetAccessString(friendContact);
+	addFriend(ap);
+}
+
+void InterestNetwork::addFriend(const pop_accesspoint &friendAccessPoint)
+{
+	_friends.push_back(friendAccessPoint);
+}
+
+void InterestNetwork::removeFriend(const std::string &friendContact)
+{
+	paroc_accesspoint ap;
+	ap.SetAccessString(friendContact);
+	removeFriend(ap);
+}
+
+void InterestNetwork::removeFriend(const pop_accesspoint &friendAccessPoint)
+{
+	_friends.remove(friendAccessPoint);
+}
+
+const std::list<pop_accesspoint> InterestNetwork::getFriends() const
+{
+	return _friends;
+}
+
+InterestNetworkCollection::InterestNetworkCollection()
+{}
+
+InterestNetworkCollection::InterestNetworkCollection(const std::list<InterestNetwork>& interests)
+{
+	_interests = interests;
+}
+
+std::list<InterestNetwork> InterestNetworkCollection::get() const
+{
+	return _interests;
+}
+
+
+void InterestNetworkCollection::set(const std::list<InterestNetwork>& insterests)
+{
+	_interests = insterests;
+}
+
+void InterestNetworkCollection::Serialize(pop_buffer &buf, bool pack)
+{
+	/* serialize */
+	if(pack)
+	{
+		int interestNb = _interests.size();
+		buf.Pack(&interestNb, 1);
+		for(std::list<InterestNetwork>::iterator i = _interests.begin(); i != _interests.end() ; i++)
+		{
+			i->Serialize(buf, pack);
+		}
+	}
+
+	/* unserialize */
+	else
+	{
+		int interestNb;
+		buf.UnPack(&interestNb, 1);
+		_interests.clear();
+		for(int i = 0; i < interestNb; i++)
+		{
+			InterestNetwork interest;
+			interest.Serialize(buf, pack);
+			_interests.push_back(interest);
+		}
+	}
+}
+
+bool InterestNetworkCollection::loadFromConfiguration(const char* file)
+{
+	pugi::xml_document doc;
+
+	/* Loading the configuration of the networks */
+	pugi::xml_parse_result result = doc.load_file(file);
+
+	/* If an error occurred */
+	if (!result)
+	{
+		return false;
+	}
+
+	/* Loading the networks */
+	pugi::xml_node rootNode = doc.child("interests");
+	for (pugi::xml_node interestNode = rootNode.child("interest"); interestNode; interestNode = interestNode.next_sibling("interest"))
+	{
+		std::string id = interestNode.child_value("id");
+		InterestNetwork interest;
+		interest.setId(id);
+
+		// friends
+		for (pugi::xml_node friendNode = interestNode.child("friend"); friendNode; friendNode = friendNode.next_sibling("friend"))
+		{
+			std::string friendInfo = friendNode.text().get();
+			interest.addFriend(friendInfo);
+		}
+
+		_interests.push_back(interest);
+	}
+
+	return true;
+}
+
+bool InterestNetworkCollection::saveToConfiguration(const char* file)
+{
+	pugi::xml_document doc;
+	pugi::xml_node rootNode = doc.append_child("interests");
+
+	for(std::list<InterestNetwork>::iterator i = _interests.begin(); i != _interests.end(); i++)
+	{
+		pugi::xml_node interestNode = rootNode.append_child("interest");
+		interestNode.append_child("id").append_child(pugi::node_pcdata).set_value(i->getId().c_str());
+
+		std::list<pop_accesspoint> friends = i->getFriends();
+		for(std::list<pop_accesspoint>::iterator f = friends.begin(); f != friends.end(); f++)
+		{
+			interestNode.append_child("friend").append_child(pugi::node_pcdata).set_value(f->GetAccessString());
+		}
+	}
+	return doc.save_file(file);
+}
+
